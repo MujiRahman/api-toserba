47d6a44728af7c00b85fd1a2b509c08d2eff29f1
MujiRahman <mujirahman888@gmail.com>
2021-12-04 09:18:53 +0700

    ...

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require ('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors')
 require('dotenv/config')
 const app = express();
 app.use(morgan('dev'))
+
 app.use(cors())
 
 const Image = require('./src/models/Image')
